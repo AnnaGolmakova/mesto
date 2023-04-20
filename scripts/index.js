adc16b83bcf0db1dbd42eb2a1a458780f2ed8b06
Anna Golmakova <ann.golmakova@gmail.com>
2023-04-21 00:03:26 +0500

    создала попап и прописала его в js

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,90 @@
-let popup = document.querySelector(".popup");
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsList = document.querySelector('.cards');
+
+function createCard(place) {
+  const card = cardTemplate.cloneNode(true);
+
+  card.querySelector('.card__image').src = place.link;
+  card.querySelector('.card__image').alt = place.name;
+  card.querySelector('.card__title').textContent = place.name;
+
+  cardsList.append(card);
+}
+
+initialCards.forEach(createCard)
+
 let page = document.querySelector("body");
+let popupEdit = document.querySelector(".popup_edit");
+let popupAdd = document.querySelector(".popup_add");
 let profileTitle = document.querySelector(".profile__info-title");
 let profileSubtitle = document.querySelector(".profile__info-subtitle");
 
 // Обработчик закрытия попапа
 function closePopup() {
   page.style.removeProperty("overflow");
-  popup.classList.remove("popup_opened");
+  document.querySelector(".popup_opened").classList.remove("popup_opened");
 }
 
 // Обработчик открытия попапа
-function openPopup() {
+function openPopup(evt) {
   page.style.overflow = "hidden";
-  nameInput.value = profileTitle.textContent;
-  jobInput.value = profileSubtitle.textContent;
-  popup.classList.add("popup_opened");
+
+  if (evt.target.className === "edit-button") {
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
+    popupEdit.classList.add("popup_opened");
+  }
+  if (evt.target.className === "add-button") {
+    popupAdd.classList.add("popup_opened");
+  }
 }
 
 let editButton = document.querySelector(".edit-button");
 editButton.addEventListener("click", openPopup);
 
-let closeButton = document.querySelector(".popup__close");
-closeButton.addEventListener("click", closePopup);
+let addButton = document.querySelector(".add-button");
+addButton.addEventListener("click", openPopup);
+
+let closeButtons = document.querySelectorAll(".popup__close");
+closeButtons.forEach((element) => {
+  element.addEventListener("click", closePopup);
+});
+
+let likeButton = document.querySelectorAll(".card__like-button");
+likeButton.forEach((element) => {
+  element.addEventListener("click", (evt) => {
+    alert("Лайк!")
+  });
+});
 
 let formElement = document.querySelector(".form");
-
 let nameInput = formElement.querySelector(".form__input_title");
 let jobInput = formElement.querySelector(".form__input_subtitle");
 
@@ -51,3 +111,34 @@ function handleFormSubmit (evt) {
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
+
+//Обработчик создания карточки
+function handleCardCreate (evt) {
+  evt.preventDefault();
+
+  if (placeTitle.value.length === 0 && placeUrl.value.length === 0) {
+    alert("Заполните название и добавьте ссылку");
+    return;
+  }
+  if (placeUrl.value.length === 0) {
+    alert("Добавьте ссылку");
+    return;
+  }
+  if (placeTitle.value.length === 0) {
+    alert("Заполните название");
+    return;
+  }
+
+  createCard({
+    name: placeTitle.value,
+    link: placeUrl.value
+  })
+  closePopup();
+}
+
+let formPlaceCreation = document.getElementsByName("create-place")[0];
+let placeTitle = formPlaceCreation.querySelector(".form__input_title");
+let placeUrl = formPlaceCreation.querySelector(".form__input_subtitle");
+
+formPlaceCreation.addEventListener('submit', handleCardCreate);
+
