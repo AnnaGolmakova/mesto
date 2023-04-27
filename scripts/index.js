765414277b7a15a902849cb0260c9584e5b5a383
Anna Golmakova <ann.golmakova@gmail.com>
2023-04-27 20:15:49 +0500

    поправила функцию openPopup/closePopup

--- scripts/index.js
+++ scripts/index.js
@@ -39,37 +39,35 @@ const profileTitle = document.querySelector(".profile__info-title");
 const profileSubtitle = document.querySelector(".profile__info-subtitle");
 
 // Обработчик закрытия попапа
-function closePopup() {
+function closePopup(popupElement) {
   page.style.removeProperty("overflow");
-  document.querySelector(".popup_opened").classList.remove("popup_opened");
-  document.querySelector(".popup_opened").setAttribute('aria-hidden', 'true');
+  popupElement.classList.remove("popup_opened");
+  popupElement.setAttribute('aria-hidden', 'true');
 }
 
-// Обработчик открытия попапа
-function openEditPopup(evt) {
+function openPopup(popupElement) {
+  popupElement.classList.add('popup_opened');
+  popupElement.setAttribute('aria-hidden', 'false');
   page.style.overflow = "hidden";
-  popupEdit.classList.add("popup_opened");
-  popupEdit.setAttribute('aria-hidden', 'false');
+}
 
 // Обработчики открытия попапа
 
 function openPreview (place) {
-  page.style.overflow = "hidden";
-  popupPreview.classList.add("popup_opened");
-  popupPreview.setAttribute('aria-hidden', 'false');
+  openPopup(popupPreview);
   popupPreview.querySelector('.popup__image').src = place.link;
   popupPreview.querySelector('.popup__image').alt = place.name;
   popupPreview.querySelector('.popup__caption').textContent = place.name;
 }
 
+function openEditPopup(evt) {
+  openPopup(popupEdit);
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
 }
 
 function openAddPopup(evt) {
-  page.style.overflow = "hidden";
-  popupAdd.classList.add("popup_opened");
-  popupAdd.setAttribute('aria-hidden', 'false');
+  openPopup(popupAdd);
   placeTitle.value = "";
   placeUrl.value = "";
 }
@@ -80,13 +78,23 @@ editButton.addEventListener("click", openEditPopup);
 const addButton = document.querySelector(".add-button");
 addButton.addEventListener("click", openAddPopup);
 
-const closeButtons = document.querySelectorAll(".popup__close");
-closeButtons.forEach((element) => {
-  element.addEventListener("click", closePopup);
+const closePreviewButton = popupPreview.querySelector(".popup__close");
+closePreviewButton.addEventListener("click", () => {
+  closePopup(popupPreview)
+});
+
+const closeEditPopupButton = popupEdit.querySelector(".popup__close");
+closeEditPopupButton.addEventListener("click", () => {
+  closePopup(popupEdit)
+});
+
+const closeAddPopupButton = popupAdd.querySelector(".popup__close");
+closeAddPopupButton.addEventListener("click", () => {
+  closePopup(popupAdd)
 });
 
 // Обработчик «отправки» формы
-function handleFormSubmit (evt) {
+function handleProfileFormSubmit (evt) {
   evt.preventDefault();
 
   if (nameInput.value.length === 0 && jobInput.value.length === 0) {
@@ -111,7 +119,7 @@ const formEditProfile = document.getElementsByName("edit-profile")[0];
 const nameInput = formEditProfile.querySelector(".form__input_title");
 const jobInput = formEditProfile.querySelector(".form__input_subtitle");
 
-formEditProfile.addEventListener('submit', handleFormSubmit);
+formEditProfile.addEventListener('submit', handleProfileFormSubmit);
 
 //Обработчик создания карточки
 function handleCardCreate (evt) {
